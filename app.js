d3ae7aae32af64b2a8c19b31b3411d2592b45e50
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-01-23 16:08:31 -0800

    appended dog details div to dom and added classlists

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ import { getDogs } from './fetch-utils.js';
 import { renderDogCard } from './render-utils.js';
 
 const dogListContainer = document.getElementById('dog-list-container');
-
+let dogArray = [];
 // on load
 window.addEventListener('load', async () => {
     // fetch all dogs
     const dogs = await getDogs();
+    dogArray = dogs;
     // render and append all dog cards to the container
     for (let dog of dogs) {
         const dogEl = renderDogCard(dog);
--- detail/detail.js
+++ detail/detail.js
@@ -2,7 +2,7 @@ import { getDog } from '../fetch-utils.js';
 import { renderDogDetail } from '../render-utils.js';
 
 const dogDetailContainer = document.getElementById('dog-detail-container');
-
+let dogResults = {};
 // on load
 window.addEventListener('load', async () => {
     // get the id from URL
@@ -10,5 +10,9 @@ window.addEventListener('load', async () => {
     const id = params.get('id');
     // use the id to fetch the dog
     const dog = await getDog(id);
+    dogResults = dog[0];
+    console.log(dogResults);
     // render and append this dog's details to the container
+    const dogChoice = renderDogDetail(dogResults);
+    dogDetailContainer.append(dogChoice);
 });
